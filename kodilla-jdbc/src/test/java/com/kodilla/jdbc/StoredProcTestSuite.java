8cd5768a1dfe1969fc9ee90523773fd02489e0ad
zxkuba <jakub.krzeszowiak@gmail.com>
2020-07-03 20:40:47 +0200

    exercise 30.3

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -29,4 +29,25 @@ public class StoredProcTestSuite {
         }
         assertEquals(0, howMany);
     }
+
+    @Test
+    public void testUpdateBestsellers() throws SQLException{
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER=\"szmira\"";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+        //When
+        String sqlUpdateTable = "CALL UpdateBestsellers()";
+        statement.execute(sqlUpdateTable);
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER=null";
+        ResultSet resultSet = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if(resultSet.next()){
+            howMany = resultSet.getInt("HOW_MANY");
+        }
+        assertEquals(0, howMany);
+
+    }
 }
